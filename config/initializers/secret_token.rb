85242376a7f5ac8418670fdfff55d4520dbb7964
Emily Ellison <emilyellison986@gmail.com>
2012-10-17 11:57:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheKordtCo::Application.config.secret_token = '0883b7fa86c14d7ab56257d7604bd799ea7b7709191704ed45c6f20d7310188d9c1dcee4dd383091faaeb4b127ba46521f7a9b0f090c40068f685e862a15b2c5'
