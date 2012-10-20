ac97503d27358ff2150449e530e948c49901c4ac
Emily Ellison <emilyellison986@gmail.com>
2012-10-20 14:48:19 -0500

    Add validations to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ActiveRecord::Base
   
   belongs_to :commentable, polymorphic: true
   
+  validates :commentable_id, presence: true
+  validates :commentable_type, presence: true
+  validates :name, presence: true
+  validates :message, presence: true
+  
 end
